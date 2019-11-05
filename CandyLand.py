3ee4d47803cf3812a9c8a8896619bb4d0367c98f
dammara <h3246531>
2019-11-04 21:23:08 -0500

    11/4/19 - 9:22PM - made small changes to keep the code consistent

--- CandyLand.py
+++ CandyLand.py
@@ -7,9 +7,10 @@ position = [1]
 
 def play():
     play1 = input(">>>").title()
-    if play1 == "Yes" or play1 == "Y":
+    if play1 == "Yes" or play1 == "Y" or "yes" or 'y':
         print("Alright, lettuce get this show on the road!")
-    elif play1 == "No" or play1 == "N":
+        input("Press ENTER")
+    elif play1 == "No" or play1 == "N" or "n" or "no":
         print("Lml, noob!XD")
         exit()
     else:
@@ -33,6 +34,8 @@ def rules():
 
 def candy_land():
     print("\n")
+    print(f">>You are now at tile {position[0]}<<")
+
     while position[0] <= 0:
         print("Game over!")
         print("You Lost!")
@@ -60,16 +63,17 @@ def candy_land():
         candy_land()
 
     while position[0] == 19:
-        print("LML, you couldn't have been more unlucky")
+        print("Fuck!, you couldn't have been more unlucky")
         print("No way did you just land on tile 19")
         position.clear()
         position.append(1)
         print("You've just been moved to tile 1...")
-        print("You have to start all over")
+        print("You have to start all over HAHA! Loser.")
         input("Press Enter")
         candy_land()
 
     while position[0] == 20:
+        print(f"Congrats, LOSER.")
         print("You have reached the end.")
         print("You've put up a valiant fight and your luck has paid off")
         print("Now, it's time to take your prize")
@@ -80,22 +84,68 @@ def candy_land():
             print("Okay, your prizes are:")
             print(prize_list1[0], "\b,", prize_list1[1], "\b, and", prize_list1[2])
             print("Have fun with those great prizes.")
-            exit()
+            print("Would you like to play again?")
+            again = input(">>>").title()
+
+            if again == "Yes" or again == "Y" or "y" or "yes":
+                print("Copy, let's do it again!")
+                position.clear()
+                position.append(1)
+                input("Press Enter")
+                candy_land()
+            else:
+                print("""
+                Wow, what a loser!
+                Why would you not want to play again after losing?
+                I thought you had a gambling addiction!
+                """)
+                exit()
 
         elif pick > pr:
             print("Okay your prizes are:")
             print(prize_list2[0], "\b,", prize_list2[1], "\b, and", prize_list2[2])
             print("Have fun with those great prizes.")
-            exit()
+            print("Would you like to play again?")
+            again = input(">>>").title()
+
+            if again == "Yes" or again == "Y" or "y" or "yes":
+                print("Copy, let's do it again!")
+                position.clear()
+                position.append(1)
+                input("Press Enter")
+                candy_land()
+            else:
+                print("""
+                Wow, what a loser!
+                Why would you not want to play again after losing?
+                I thought you had a gambling addiction!
+                """)
+                exit()
 
         else:
             print("Lucky you. You get two both prize lists.")
             print("Both :", prize_list2[0], "\b,", prize_list2[1], "\b,", prize_list2[2], "\b,", prize_list1[0], "\b,", prize_list1[1], "\b, and", prize_list1[2], "!!!")
             print("Have fun with those awesome prizes.")
-            exit()
+            print("Would you like to play again?")
+            again = input(">>>").title()
+
+            if again == "Yes" or again == "Y" or "y" or "yes":
+                print("Copy, let's do it again!")
+                position.clear()
+                position.append(1)
+                input("Press Enter")
+                candy_land()
+            else:
+                print("""
+                Wow, what a loser!
+                Why would you not want to play again after losing?
+                I thought you had a gambling addiction!
+                """)
+                exit()
 
     roll = roll_dice()
     print(f"You rolled a {roll}")
+
     if roll == 1 or roll == 3:
         print("You have been moved back one space.")
         position[0] -= 1
@@ -106,37 +156,44 @@ def candy_land():
     elif roll == 5:
         print("You have been moved back two spaces.")
         position[0] -= 2
-        print("Your position is now", position[0])
+        print("Your position is now tile", position[0])
         input("Press Enter to continue")
         candy_land()
 
     elif roll == 2 or roll == 4:
         print("You have been moved forward one space.")
         position[0] += 1
-        print("Your position is now", position[0])
+        print("Your position is now tile", position[0])
         input("Press Enter")
         candy_land()
 
     elif roll == 6 or roll == 8:
         print("You have been moved forward two spaces!!!!")
         position[0] += 2
-        print("Your position is now", position[0])
+        print("Your position is now tile", position[0])
         input("Press Enter")
         candy_land()
 
     elif roll == 7:
-        print("You will have to roll again.")
+        print("Ayyy, lucky 7!")
+        print("You get to roll again!")
+        input("Press ENTER to roll again.")
         candy_land()
 
 
 print("Hello user, what is your name?")
 name = input(">>>").title()
 print(f"Ahh, nice to meet you {name}.")
-print("My name is...")  # Make up a name M.
+print(f"My name is also {name} and I like to steal people's identities")
+print(f"I think instead of {name}, I'm gonna call you 'LOSER'.")
 print("Would you like to play a game? *Yes or No*")
 play()
 print(f"""This game is called Candy Land.
-Let me explain the rules to you, {name}.""")
+Let me explain the rules to you, LOSER.""")
+input("Press ENTER to see da rools")
 rules()
 input("Press Enter when you have read the rules to move on")
+print("\n")
+print("So basically, you're not doing Ecstasy, but you will be rolling a lot.")
+input("Press ENTER to begin!")
 candy_land()
